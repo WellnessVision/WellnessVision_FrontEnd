cc961978c4b14c2d281e76755910b58c9604d509
RuchithSathnidu <ruchithsathnidu123@gmail.com>
2024-06-11 19:35:10 +0530

    Create react app and setup the file structure

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import FirstPage from './pages/FirstPage'
+import 'bootstrap/dist/css/bootstrap.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "page",
+    element: <FirstPage/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+   <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/pages/FirstPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FirstPage() {
+  return (
+    <div className="alert alert-danger" role="alert">
+  A simple danger alert—check it out!
+</div>
+  )
+}
+
+export default FirstPage
