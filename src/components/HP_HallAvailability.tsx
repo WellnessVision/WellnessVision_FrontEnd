f9f4640384de5a1ec8e6d86fe6ec6318d057bc32
RuchithSathnidu <ruchithsathnidu123@gmail.com>
2024-06-20 14:26:20 +0530

    Do some modifications to health professionals hall availability controller and addPhysicalEvent page

--- src/components/HP_HallAvailability.tsx
+++ src/components/HP_HallAvailability.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+import axios from 'axios';
 import '../pages/HP/HP_AddPhysicalEvent.css';
 import './HP_HallAvailability.css';
 import '../pages/HP/HP_OneEvent.css';
@@ -6,11 +7,60 @@ import '../pages/HP/HP_OneEvent.css';
 interface HP_HallAvailabilityProps {
   show_2: boolean;
   handleClose_2: () => void;
-  eventData: any; // Adjust type as per your actual data structure
+  eventData: any;
+  finalDuration: number;
+  eventImage: File | null; 
 }
 
-const HP_HallAvailability: React.FC<HP_HallAvailabilityProps> = ({ show_2, handleClose_2, eventData }) => {
+const HP_HallAvailability: React.FC<HP_HallAvailabilityProps> = ({ show_2, handleClose_2, eventData, finalDuration, eventImage}) => {
   const [message, setMessage] = useState('');
+  const [event_id, setEventId] = useState('');
+
+  useEffect(() => {
+    if (eventData && eventData.event_id) {
+      setEventId(eventData.event_id);
+    }
+  }, [eventData]);
+
+  const handleOrderPhysicalEvent = async (event: FormEvent) => {
+    event.preventDefault();
+    if (event_id) {
+      try {
+
+        if (!eventImage) {
+          setMessage('Please select an image file');
+          return;
+      }
+
+        const formData = new FormData();
+        formData.append('file', eventImage);
+        formData.append('event_id', event_id);
+
+        await axios.post('http://localhost:15000/physicalEventImageUpload', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+        handleClose_2();
+      } catch (error) {
+        setMessage('Error registering event');
+      }
+    }
+  };
+
+  const handleCancelPhysicalEvent = async (event: FormEvent) => {
+    event.preventDefault();
+    if (event_id) {
+      try {
+        await axios.put('http://localhost:15000/physicalEvent', {
+          event_id
+        });
+        handleClose_2();
+      } catch (error) {
+        setMessage('Error registering event');
+      }
+    }
+  };
 
   return (
     <div className={`popup ${show_2 ? 'show' : ''} popup_HP_HallAvailability_popup`}>
@@ -23,22 +73,46 @@ const HP_HallAvailability: React.FC<HP_HallAvailabilityProps> = ({ show_2, handl
               {eventData ? (
                 <div>
                   <p className="card-text detail Hall_ID_HP_HallAvailability"><i className="bi bi-award-fill"></i> {eventData.hall_id} (Hall ID)</p>
-                  <p className="card-text detail Hall_type_HP_HallAvailability"><i className="bi bi-bookmark-star-fill"></i> {eventData.hall_type}</p>
+                  {eventData.hall_type === 'Lecture' && (
+                    <p className="card-text detail Hall_type_HP_HallAvailability">
+                      <i className="bi bi-bookmark-star-fill"></i> {eventData.hall_type} Hall (Tables and Chairs)
+                    </p>
+                  )}
+                  {eventData.hall_type === 'Therapy' && (
+                    <p className="card-text detail Hall_type_HP_HallAvailability">
+                      <i className="bi bi-bookmark-star-fill"></i> {eventData.hall_type} Hall (Therapy Beds)
+                    </p>
+                  )}
+                  {eventData.hall_type === 'Free Space' && (
+                    <p className="card-text detail Hall_type_HP_HallAvailability">
+                      <i className="bi bi-bookmark-star-fill"></i> {eventData.hall_type} Hall (Yoga Mats)
+                    </p>
+                  )}
                   <p className="card-text detail seats_HP_HallAvailability"><i className='bi bi-person-workspace'></i> {eventData.capacity} Seats (Maximum Participant Capacity)</p>
-                  <p className="card-text detail price_HP_HallAvailability"><i className='bi bi-cash-stack'></i> Rs.{eventData.charge} (Per Hour (2h))</p>
-                  <p className="card-text detail duration_HP_HallAvailability"><i className='bi bi-hourglass-split'></i> {eventData.charge} * 2h = Rs.{parseInt(eventData.charge)*2} (Total Charge for Hall)</p>
-                </div>
-              ) : (
-                <p>No event data available</p>
-              )}
-              <div>
-                <a href="HP_OneEvent" className="btn btn-primary HP_HallAvailability_cancel">
+                  <p className="card-text detail price_HP_HallAvailability"><i className='bi bi-cash-stack'></i> Rs.{eventData.charge}/= (Per Hour (1h))<span className="card-text detail duration_HP_HallAvailability"><i className='bi bi-hourglass-split'></i> {finalDuration} hour duration</span></p>
+                  <p className="card-text detail duration_HP_HallAvailability"><i className='bi bi-cash-stack'></i> Rs.{eventData.charge}/= *  {finalDuration}h = Rs.{parseInt(eventData.charge)*finalDuration}/= (Total Charge for Hall)</p>
+                  
+                  <div className='HP_HallAvailability_button_div'>
+                <a className="btn btn-primary HP_HallAvailability_cancel_button" onClick={handleCancelPhysicalEvent}>
                   <i className="bi bi-arrow-left-circle"></i> Cancel
                 </a>
-                <a href="HP_OneEvent" className="btn btn-warning HP_HallAvailability_hallBook">
+                <a href="HP_ViewEvents" className="btn btn-warning HP_HallAvailability_hallBook" onClick={handleOrderPhysicalEvent}>
                   <i className="bi bi-bag-plus-fill"></i><span className='HP_HallAvailability_hallBook_text'> Book Now</span>
                 </a>
               </div>
+                </div>
+                
+              ) : (
+                <div>
+                  <p className='Unfortunately_header_HP_HallAvailability'>Unfortunately, Not Available</p>
+                  <p className='Unfortunately_text_HP_HallAvailability'>All halls that meet your specifications are already booked for that date.</p>
+                  <div className='HP_HallAvailability_button_div'>
+                <a className="btn btn-primary HP_HallAvailability_cancel_button" onClick={handleClose_2}>
+                  <i className="bi bi-arrow-left-circle"></i> Go Back
+                </a>
+              </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
